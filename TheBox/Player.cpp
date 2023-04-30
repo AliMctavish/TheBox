ffc5607ccc2c69e5f29f29c23e8463e994ccec16
ali fadil <alifadil4141@gmail.com>
2023-04-30 22:23:48 +0300

    idk

---
+++ TheBox/Player.cpp
@@ -0,0 +1,13 @@
+#include "Player.h"
+
+
+Player::Player(float x , float y):x(playerPos.x),y(playerPos.y)
+{
+
+}
+
+
+
+
+
+
---
+++ TheBox/Player.h
@@ -0,0 +1,22 @@
+#pragma once
+
+class Player {
+
+
+	Player();
+
+
+	~Player();
+
+
+
+	void initVariables();
+
+	void Update();
+
+
+	void render();
+
+
+
+};
